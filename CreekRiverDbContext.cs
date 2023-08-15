1ab18d6edd224494710fcf662f096e79048752ff
Kyle Blunt <kbluntaudio@gmail.com>
2023-08-14 21:20:03 -0500

    Implemented initial data

--- CreekRiverDbContext.cs
+++ CreekRiverDbContext.cs
@@ -13,4 +13,34 @@ public class CreekRiverDbContext : DbContext
     {
 
     }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // seed data with campsite types
+        modelBuilder.Entity<CampsiteType>().HasData(new CampsiteType[]
+        {
+            new CampsiteType {Id = 1, CampsiteTypeName = "Tent", FeePerNight = 15.99M, MaxReservationDays = 7},
+            new CampsiteType {Id = 2, CampsiteTypeName = "RV", FeePerNight = 26.50M, MaxReservationDays = 14},
+            new CampsiteType {Id = 3, CampsiteTypeName = "Primitive", FeePerNight = 10.00M, MaxReservationDays = 3},
+            new CampsiteType {Id = 4, CampsiteTypeName = "Hammock", FeePerNight = 12M, MaxReservationDays = 7}
+        });
+
+        modelBuilder.Entity<Campsite>().HasData(new Campsite[]
+        {
+            new Campsite {Id = 1, CampsiteTypeId = 1, Nickname = "Barred Owl", ImageUrl="https://tnstateparks.com/assets/images/content-images/campgrounds/249/colsp-area2-site73.jpg"},
+            new Campsite {Id = 2, CampsiteTypeId = 2, Nickname = "Long Creek", ImageUrl="https://tnstateparks.com/assets/images/content-images/campgrounds/249/colsp-area2-site73.jpg"},
+            new Campsite {Id = 3, CampsiteTypeId = 3, Nickname = "Hunter Row", ImageUrl="https://tnstateparks.com/assets/images/content-images/campgrounds/249/colsp-area2-site73.jpg"},
+            new Campsite {Id = 4, CampsiteTypeId = 4, Nickname = "Laying Deer", ImageUrl="https://tnstateparks.com/assets/images/content-images/campgrounds/249/colsp-area2-site73.jpg"},
+            new Campsite {Id = 5, CampsiteTypeId = 5, Nickname = "Howling Wolf", ImageUrl="https://tnstateparks.com/assets/images/content-images/campgrounds/249/colsp-area2-site73.jpg"},
+        });
+        
+        modelBuilder.Entity<UserProfile>().HasData(new UserProfile[]
+        {
+            new UserProfile {Id = 1, FirstName = "Kyle", LastName = "Blunt", Email = "testblunt1020@gmail.com"},
+        });
+        
+        modelBuilder.Entity<Reservation>().HasData(new Reservation[]
+        {
+            new Reservation {Id = 1, CampsiteId = 1, UserProfileId = 1, CheckinDate = DateTime.Today, CheckoutDate = DateTime.Today.AddDays(7) },
+        });
+    }
 }
